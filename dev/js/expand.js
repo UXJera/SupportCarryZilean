9d43f4627d4acd0c707a445beb68e9fb79d3d30d
jera <jeremiahnyman@gmail.com>
2016-01-12 12:47:48 -0600

    Initial Upload of website
    1/12 Upload of website, working on Lane Matchups

---
+++ dev/js/expand.js
@@ -0,0 +1,33 @@
+$(".expand").click(function () {
+
+    $expand = $(this);
+    //getting the next element
+    $content = $expand.next();
+    //open up the content needed - toggle the slide- if visible, slide up, if not slidedown.
+    $content.slideToggle(500, function () {
+        //execute this after slideToggle is done
+        //change text of header based on visibility of content div
+        $expand.text(function () {
+            //change text based on condition
+            return $content.is(":visible") ? "Hide Explanation" : "Show Explanation";
+        });
+    });
+
+});
+
+$(".skillhide").click(function () {
+
+    $skillhide = $(this);
+    //getting the next element
+    $content = $skillhide.next();
+    //open up the content needed - toggle the slide- if visible, slide up, if not slidedown.
+    $content.slideToggle(500, function () {
+        //execute this after slideToggle is done
+        //change text of header based on visibility of content div
+        $skillhide.text(function () {
+            //change text based on condition
+            return $content.is(":visible") ? "Hide Full Skill Order" : "Show Full Skill Order";
+        });
+    });
+
+});
